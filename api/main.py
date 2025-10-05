f3aea78aa2387adb85a6cbd2aeceec41458d7531
robecm <robecm08@gmail.com>
2025-10-05 09:50:20 -0600

    Update Dockerfile: replace libgl1-mesa-glx with libgl1

--- api/main.py
+++ api/main.py
@@ -222,4 +222,4 @@ def get_lightcurve(mission: str, ID: int):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
