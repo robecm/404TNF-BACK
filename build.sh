cba03377da09039d6fe2e7bd59fdda4d254a4062
robecm <robecm08@gmail.com>
2025-10-05 01:41:37 -0600

    Add build script for system and Python dependencies in Vercel configuration

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
+#!/bin/bash
+
+# This script runs in the Vercel build environment
 
 echo "--- Installing system dependencies (libgomp) ---"
 yum install -y libgomp
 
 echo "--- Installing Python dependencies ---"
-pip install -r requirements.txt
+# Use the python3.12 executable to run pip
+python3.12 -m pip install -r requirements.txt
 
 echo "--- Build finished ---"
